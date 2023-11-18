1f7073f2bab425a86b21fae43a9b36bb772ae587
Michael Johnson <mcangelo200@gmail.com>
2023-11-18 10:03:25 +0000

    --worked on the journal csss

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <form className="note-form">
+        <input type="text" className="note-input" placeholder='Title' required/>
+        <textarea placeholder='Content' rows={10}></textarea>
+        <button type="submit" className="note-submit"> Add Note </button>
+      </form>
+      <div className="notes-grid">
+        <div className="notes-item">
+          <div className="notes-header">
+            <button> X </button>
+          </div>
+          <h2> Title </h2>
+          <p> Content </p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
